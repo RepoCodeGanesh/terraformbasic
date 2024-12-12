f8cf28797cafbcbc7751c9e404dd55cd0ec7e266
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-13 00:04:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "vm" {
   resource_group_name   = azurerm_resource_group.rg.name
   location              = var.resource_group_location
   vm_size               = "Standard_B1ls"
-  vm_os                 = "Windows2012R2DatacenterCore"
+  vm_os                 = "Windows2019DatacenterCore"
   admin_username        = var.admin_username
   admin_password        = var.admin_password
   tags                  = var.tags
