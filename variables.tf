efb01e6fcdefaf2bb1bf0985b9141237becfe9bf
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-12 23:47:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,20 +7,31 @@ variable "resource_group_location" {
 variable "resource_group_name_prefix" {
   type        = string
   default     = "rg2"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+  description = "Prefix of the resource group name that's combined with a random ID so the name is unique in your Azure subscription."
 }
 
 variable "client_secret" { 
-description = "The client secret for the service principal"
-type = string
-sensitive = true 
+  description = "The client secret for the service principal"
+  type        = string
+  sensitive   = true 
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
 }
 
 variable "tags" {
-description = "A map of tags to assign to the resource group"
-type = map(string) 
-default = {
-Environment = "tag1"
-Dept = "tag2"
- } 
+  description = "A map of tags to assign to the resource group"
+  type        = map(string) 
+  default = {
+    Environment = "tag1"
+    Dept        = "tag2"
+  } 
 }
