9ea81283431f369f22e61363e0846b7f3234dca5
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-12 23:48:43 +0530

    Create variables.tf

---
+++ modules/vm/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the resources"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Size of the VM"
+  type        = string
+}
+
+variable "vm_os" {
+  description = "OS version for the VM"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource group"
+  type        = map(string)
+}
