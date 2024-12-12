73f586a66dad12f00d7f1c6686b77e1594d6c902
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-12 23:59:55 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -23,35 +23,9 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = var.vm_os
+    sku       = "2012-R2-Datacenter"
     version   = "latest"
   }
 
   tags = var.tags
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "nic-${var.random_pet_name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-${var.random_pet_name}"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.random_pet_name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
 }
