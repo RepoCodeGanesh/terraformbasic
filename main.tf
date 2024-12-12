9c80cc7b7f05d0571671d5f0cfd1b6820233e281
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-12 23:46:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ resource "random_pet" "rg_name" {
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
-  tags = var.tags
+  tags     = var.tags
 }
 
+module "vm" {
+  source              = "./modules/vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  vm_size             = "Standard_B1ls"
+  vm_os               = "Windows2012R2DatacenterCore"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  tags                = var.tags
+}
