852087c7d40911941db910be09c5ba69719d0bd6
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-12 23:46:50 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -14,11 +14,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "e94bdd49-97a8-440d-bf40-10569cc9409e"
-  tenant_id         = "9a998602-7371-4737-98c5-cd71dd4a9e9b"
-  client_id         = "1ce6cbf4-1110-4ec8-b98d-80088bde1942"
-  client_secret     = var.client_secret
+  subscription_id = "e94bdd49-97a8-440d-bf40-10569cc9409e"
+  tenant_id       = "9a998602-7371-4737-98c5-cd71dd4a9e9b"
+  client_id       = "1ce6cbf4-1110-4ec8-b98d-80088bde1942"
+  client_secret   = var.client_secret
 }
-
-# Your code goes here
-
