c852a9cd9c4a6236faf2bbda28958af070c0a441
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-13 23:03:47 +0530

    Update outputs.tf

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -7,3 +7,18 @@ output "vm_private_ip" {
   description = "The private IP address of the VM"
   value       = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
 }
+
+output "vm_ids" {
+  description = "The IDs of all created VMs"
+  value = [for vm in azurerm_virtual_machine.vm : vm.id]
+}
+
+output "vm_names" {
+  description = "The names of all created VMs"
+  value = [for vm in azurerm_virtual_machine.vm : vm.name]
+}
+
+output "vm_private_ips" {
+  description = "The private IP addresses of all created VMs" 
+  value = [for nic in azurerm_network_interface.nic : nic.ip_configuration[0].private_ip_address] 
+}
