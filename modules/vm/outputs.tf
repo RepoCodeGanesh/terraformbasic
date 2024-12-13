7d46ce3e105afcb13450fc4c2e1f2ba8bca914e0
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-13 23:11:23 +0530

    Update outputs.tf

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,8 +1,3 @@
-output "Vm_Name" {
-  value = azurerm_virtual_machine.vm.name
-}
-
-
 output "vm_private_ip" {
   description = "The private IP address of the VM"
   value       = azurerm_network_interface.nic.ip_configuration[0].private_ip_address
