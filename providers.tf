a61374eb568150e514c0c9a3251408f7ab1a4920
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-13 13:58:53 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,10 +12,15 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 
   subscription_id = "e94bdd49-97a8-440d-bf40-10569cc9409e"
   tenant_id       = "9a998602-7371-4737-98c5-cd71dd4a9e9b"
   client_id       = "1ce6cbf4-1110-4ec8-b98d-80088bde1942"
   client_secret   = var.client_secret
+}
 }
