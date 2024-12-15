479c1bf5c6381c3c6dad12a6b4ae9416bfceb99a
Ganesan Kasi <RichTextForGanesh@outlook.com>
2024-12-15 18:12:08 +0530

    deploy multiple vms

--- main.tf
+++ main.tf
@@ -19,3 +19,15 @@ module "vm" {
   admin_password        = var.admin_password
   tags                  = var.tags
 }
+
+module "vm" {
+  source                = "./modules/vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = var.resource_group_location
+  hostname              = var.hostname
+  vm_size               = var.vm_size
+  vm_os                 = var.vm_os
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+  tags                  = var.tags
+}
