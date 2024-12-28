ebf42dc9d4247927c133e6437dd3996a3565a45c
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-28 15:29:22 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -43,17 +43,3 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-${var.resource_group_name}"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.resource_group_name}"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
-}
