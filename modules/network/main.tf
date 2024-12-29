bcfac730c9cf0905db8bcbda6b11797e60868416
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 14:00:16 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,15 +1,15 @@
 resource "azurerm_virtual_network" "vnet" {
   count               = length(var.address_space_name)
   resource_group_name = var.resource_group_name
-  location            = var.location
+  location            = var.resource_group_location
   name                = var.address_space_name[count.index]
   address_space       = [var.address_spaces[count.index]]
 }
 
 resource "azurerm_subnet" "subnet" {
   count                = length(var.subnet_names)
-  resource_group_name   = var.resource_group_name
+  resource_group_name  = var.resource_group_name
   name                 = var.subnet_names[count.index]
   virtual_network_name = azurerm_virtual_network.vnet[count.index].name
-  address_prefixes       = var.subnet_prefixes[count.index]
+  address_prefix       = var.subnet_prefixes[count.index]
 }
