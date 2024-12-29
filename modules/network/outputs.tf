3f510a88d87b754f1ffd55c7aa2051019015870b
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 17:53:41 +0530

    Update outputs.tf

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,3 +1,9 @@
+output "vnet_ids" {
+  description = "The IDs of the VNets"
+  value       = azurerm_virtual_network.vnet.*.id
+}
+
 output "subnet_ids" {
-  value = azurerm_subnet.subnet[*].id
+  description = "The IDs of the subnets"
+  value       = azurerm_subnet.subnet.*.id
 }
