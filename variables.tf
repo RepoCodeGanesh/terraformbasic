3a2ed3d9b9b4ba98f4037d8bfaaa9f052f2d29c4
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 17:58:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -62,38 +62,15 @@ variable "hostname" {
   type = string
 }
 
-variable "address_spaces" {
-  description = "The address spaces for the virtual network"
-  type        = list(string)
-  default     = ["10.0.0.0/16", "10.2.0.0/16"]
-}
-
-variable "address_space_name" {
-  description = "The address space names for the virtual network"
-  type        = list(string)
-  default     = ["Vnet1", "Vnet2"]
-}
-
-variable "subnet_prefixes" {
-  description = "The subnet prefixes for the subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "subnet_names" {
-  description = "The names of the subnets"
-  type        = list(string)
-  default     = ["subnet1", "subnet2", "subnet3"]
-}
-
-variable "subnet_ids" {
-  description = "The subnet IDs for the network interfaces"
-  type        = list(string)
-  default = ["subnet-12345678", "subnet-87654321"]
-}
-
 variable "location" {
   type        = string
   default     = "eastus"
   description = "Location of the resource group."
+}
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "my-resource-group"
 }
