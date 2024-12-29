7a062c04489b95a4be167ad835ff11f2d582ad6b
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 20:52:20 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -42,5 +42,5 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   tags = var.tags
-  depends_on = [module.network] # Explicitly depend on the network module
+  # depends_on = [module.network] # Explicitly depend on the network module
 }
