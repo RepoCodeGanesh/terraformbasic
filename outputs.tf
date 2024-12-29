407d825a589ce77e8b5b21a671a2d318b4b12e0b
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 18:49:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,3 @@
-# output "resource_group_name" {
-#   value = azurerm_resource_group.rg.name
-# }
-
-# output "Vm_Name" {
-#   value = module.vm.vm_names
-# }
-
-# output "vm_ids" {
-#   value = module.vm.vm_ids
-# }
-
-# output "VmIP" {
-#   value = module.vm.vm_private_ips
-# }
-
-# output "vm_admin_username" {
-# description = "The admin username for the VM"
-#   value = var.admin_username 
-# }
-
-# output "vm_admin_password" {
-# description = "The admin password for the VM"
-#     value = var.admin_password
-#     sensitive = true 
-# }
-
 output "vnet_ids" {
   description = "The IDs of the VNets"
   value       = azurerm_virtual_network.vnet.*.id
