4ee10257a446acf1fc4e026876e7d03c8010967c
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 21:12:07 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "Vm_Name" {
+  value = module.vm.vm_names
+}
+
+output "vm_ids" {
+  value = module.vm.vm_ids
+}
+
+output "VmIP" {
+  value = module.vm.vm_private_ips
+}
 output "vnet_ids" {
   description = "The IDs of the VNets"
   value       = module.network.vnet_ids
