f6f055614f33dc788df2afa83a475fdc4f34f514
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 18:11:54 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -11,5 +11,5 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${element(["VM", "DB"], count.index % 2)}-subnet${count.index / 2 + 1}"
   resource_group_name  = var.resource_group_name
   virtual_network_name = element(azurerm_virtual_network.vnet.*.name, count.index / 2)
-  address_prefixes     = ["10.${count.index / 2 + 1}.${count.index % 2 * 64}.0/18"]
+  address_prefixes     = ["10.${count.index / 2 + 1}.${(count.index % 2) * 64}.0/18"]
 }
