098e5f8920f8692ff6f50bb1920f153fe78cb7e8
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 17:53:19 +0530

    Update variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,35 +1,11 @@
+variable "location" {
+  description = "The Azure region to deploy resources"
+  type        = string
+  default     = "East US"
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-}
-
-variable "resource_group_location" {
-  description = "The location of the resource group"
-  type        = string
-}
-
-variable "address_space_name" {
-  description = "The names of the virtual network address spaces"
-  type        = list(string)
-}
-
-variable "address_spaces" {
-  description = "The address spaces for the virtual network"
-  type        = list(string)
-}
-
-variable "subnet_names" {
-  description = "The names of the subnets"
-  type        = list(string)
-}
-
-variable "subnet_prefixes" {
-  description = "The subnet prefixes for the subnets"
-  type        = list(string)
-}
-
-variable "location" {
-  type        = string
-  default     = "eastus"
-  description = "Location of the resource group."
+  default     = "my-resource-group"
 }
