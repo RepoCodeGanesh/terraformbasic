1eaa94b652fc27d3525b885dd7c499678909fc06
Ganesan Kasi <RichTextForGanesh@outlook.com>
2024-12-29 15:28:37 +0530

    done

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
+# output "resource_group_name" {
+#   value = azurerm_resource_group.rg.name
+# }
 
-output "Vm_Name" {
-  value = module.vm.vm_names
-}
+# output "Vm_Name" {
+#   value = module.vm.vm_names
+# }
 
-output "vm_ids" {
-  value = module.vm.vm_ids
-}
+# output "vm_ids" {
+#   value = module.vm.vm_ids
+# }
 
-output "VmIP" {
-  value = module.vm.vm_private_ips
-}
+# output "VmIP" {
+#   value = module.vm.vm_private_ips
+# }
 
-output "vm_admin_username" {
-description = "The admin username for the VM"
-  value = var.admin_username 
-}
+# output "vm_admin_username" {
+# description = "The admin username for the VM"
+#   value = var.admin_username 
+# }
 
-output "vm_admin_password" {
-description = "The admin password for the VM"
-    value = var.admin_password
-    sensitive = true 
-}
+# output "vm_admin_password" {
+# description = "The admin password for the VM"
+#     value = var.admin_password
+#     sensitive = true 
+# }
 
-output "subnet_ids" {
-  value = azurerm_subnet.subnet[*].id
-}
+# output "subnet_ids" {
+#   value = azurerm_subnet.subnet[*].id
+# }
