a549aa66e949b33b7e1603d71b34a5fa35f7748c
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 20:53:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "vnet_ids" {
   value       = module.network.vnet.*.id
 }
 
-output "subnet_ids" {
-  description = "The IDs of the subnets"
-  value       = module.network.subnet.*.id
-}
+#output "subnet_ids" {
+#  description = "The IDs of the subnets"
+#  value       = module.network.subnet.*.id
+#}
