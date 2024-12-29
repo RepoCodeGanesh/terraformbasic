5bd5c388fcad39cee6bf795166c3eea55aad863d
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 19:58:23 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -4,6 +4,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = [var.vnet_address_spaces[count.index]]
   location            = var.location
   resource_group_name = var.resource_group_name
+  tags = var.tags
 }
 
 resource "azurerm_subnet" "subnet" {
