159a1e54872435ed3aba7d3f42129edb438606ee
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-29 18:03:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -66,8 +66,7 @@ variable "location" {
   type        = string
   default     = "eastus"
   description = "Location of the resource group."
-}
-}
+} 
 
 variable "resource_group_name" {
   description = "The name of the resource group"
