1f7de67de4309c568f1faeac091a001391d2417b
RepoCodeGanesh <161068086+RepoCodeGanesh@users.noreply.github.com>
2024-12-30 13:13:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -78,12 +78,12 @@ variable "subnet_prefixes" {
    type        = list(string)
  }
 
-variable "subnet_ids" {
-  description = "The subnet IDs for the network interfaces"
-  type        = list(string)
-}
+#variable "subnet_ids" {
+  #description = "The subnet IDs for the network interfaces"
+  #type        = list(string)
+#}
 
-variable "vm_subnet_id" {
-  description = "The subnet IDs for the network interfaces"
-  type        = string
-}
+#variable "vm_subnet_id" {
+  #description = "The subnet IDs for the network interfaces"
+  #type        = string
+#}
